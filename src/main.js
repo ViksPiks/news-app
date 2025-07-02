b6287caca60629cfc9f5b84b29d8865c3146384f
viks <victoria.miledina@gmail.com>
2025-07-02 22:19:22 +0200

    populate country select with countries

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+import { countries } from "./constants/countries";
+
+const addOptionsToSelect = (countriesList) => {
+  const select = document.querySelector("select#country");
+
+  countriesList.forEach((country) => {
+    const option = document.createElement("option");
+    option.value = country.code;
+    option.textContent = country.name;
+    select.appendChild(option);
+  });
+};
+
+addOptionsToSelect(countries);
